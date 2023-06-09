f0aae7f82745d111ce7c0e91b1a0e08252bf4a50
bakai <bisson641@gmail.com>
2023-06-09 15:36:41 +0600

    add solution for prime numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -21,7 +21,9 @@ PRIME = "prime"
 
 
 def is_prime(n):
-    if n % 2 == 0:
+    if n == 1:
+        return False
+    elif n % 2 == 0:
         return n == 2
     d = 3
     while d * d <= n and n % d != 0:
