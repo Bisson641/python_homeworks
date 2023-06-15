5a68ab7271bc7f2935afc1b67203ee3e511fc85e
Bisson641 <110412814+Bisson641@users.noreply.github.com>
2023-06-15 14:52:34 +0600

    Update main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -49,7 +49,3 @@ def filter_numbers(numbers_list, filter_type):
         return [number for number in numbers_list if number % 2 == 0]
     if filter_type == PRIME:
         return [number for number in numbers_list if is_prime(number)]
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
